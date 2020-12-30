fc376b21475b6a90ce9ae6cc9b7577f5d9267964
Felipe Barros <bcfelipe@icloud.com>
2020-12-30 20:25:34 -0300

    Projeto inicial com leitura básica do livro

---
+++ src/screens/Home/style.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#000'
+    },
+    content: {
+        flex: 1
+    },
+    footer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingHorizontal: 20,
+        paddingVertical: 10
+    }
+})

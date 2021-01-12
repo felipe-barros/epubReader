11d8d8e37d4ed96253f475b1032f00e45505b870
Felipe Barros <bcfelipe@icloud.com>
2021-01-12 11:49:20 -0300

    Nova interface de leitura

---
+++ .history/src/screens/Home/index_20210112104617.js
@@ -0,0 +1,220 @@
+import React, { useRef, useState } from 'react';
+import { Button, FlatList, Modal, SafeAreaView, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { WebView } from 'react-native-webview';
+import html from '../../templates/index.html';
+import themeToStyles from '../../utils/themeToStyles';
+import style from './style';
+
+
+const lightMode = {
+    bg: '#FFF',
+    fg: '#000',
+    size: '100%',
+};
+
+const darkMode = {
+    bg: '#000 !important',
+    fg: '#FFF !important',
+    size: '100%',
+}
+
+function Home() {
+    const webview = useRef();
+    const fontSizes = ["25%", "50%", "75%", "100%", "125%", "150%", "175%", "200%"];
+    const [fontSizeIndex, setFontSizeIndex] = useState(3); // Tamanho de fonte original (100%)
+    const [theme, setTheme] = useState({
+        lightMode
+    })
+    const [cl, setCl] = useState(null);
+    const [searchResults, setSearchResults] = useState([]);
+    const [search, setSearch] = useState('');
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const [isDarkMode, setIsDarkMode] = useState(false);
+    const [lastMarkedCfi, setLastMarkedCfi] = useState("");
+
+    let injectedJS = `window.BOOK_PATH = "../books/book2.epub"; window.THEME = ${JSON.stringify(themeToStyles(theme))};`;
+    if (cl) {
+        injectedJS = `${injectedJS}
+		window.BOOK_LOCATION = "${cl}";
+		`;
+    }
+    function goPrev() {
+        webview.current?.injectJavaScript(`window.rendition.prev(); true`);
+    }
+
+    function goNext() {
+        webview.current?.injectJavaScript(`window.rendition.next(); true`);
+    }
+
+    function refresh() {
+        webview.current?.injectJavaScript(`window.BOOK_LOCATION = "${cl}"`);
+        webview.current?.reload();
+    }
+
+    function goToLocation(href) {
+        webview.current?.injectJavaScript(`
+        window.rendition.display('${href}'); 
+        window.rendition.annotations.remove("${lastMarkedCfi}", "highlight");
+        window.rendition.annotations.highlight("${href}", {}, (e) => {
+            console.log("highlight clicked", e.target);
+        }, "", {"fill": "dodgerblue"});
+        true`);
+        setLastMarkedCfi(href);
+        setIsModalVisible(false);
+    }
+
+    function decreaseFontSize() {
+        var newFontSizeIndex = fontSizeIndex;
+
+        if (fontSizeIndex > 0) {
+            newFontSizeIndex = fontSizeIndex - 1;
+            setFontSizeIndex(newFontSizeIndex);
+        }
+
+        var newTheme = theme;
+        newTheme.size = fontSizes[newFontSizeIndex];
+
+        setTheme(newTheme);
+        refresh();
+    }
+
+    function increaseFontSize() {
+        var newFontSizeIndex = fontSizeIndex;
+
+        if (fontSizeIndex < (fontSizes.length - 1)) {
+            newFontSizeIndex = fontSizeIndex + 1;
+            setFontSizeIndex(newFontSizeIndex);
+        }
+
+        var newTheme = theme;
+        newTheme.size = fontSizes[newFontSizeIndex];
+
+        setTheme(newTheme);
+        refresh();
+    }
+
+    function goSearch() {
+        webview.current?.injectJavaScript(`
+		Promise.all(
+			window.book.spine.spineItems.map((item) => {
+				return item.load(window.book.load.bind(window.book)).then(() => {
+					let results = item.find('${search}'.trim());
+					item.unload();
+					return Promise.resolve(results);
+				});
+			})
+		).then((results) =>
+			window.ReactNativeWebView.postMessage(
+				JSON.stringify({ type: 'search', results: [].concat.apply([], results) })
+			)
+		); true`);
+    }
+
+    function handleMessage(msg) {
+        let parsedData = JSON.parse(msg.nativeEvent.data);
+        let { type } = parsedData;
+
+        delete parsedData.type;
+
+        switch (type) {
+            case 'search':
+                const results = parsedData.results;
+                if (results.length > 0) {
+                    setSearchResults(results)
+                    setIsModalVisible(true);
+                }
+                return;
+            case 'loc':
+                setCl(parsedData.location);
+                return;
+            default:
+                return;
+        }
+    }
+
+    function goDarkMode() {
+        if (!isDarkMode) {
+            var newTheme = darkMode;
+            newTheme.size = fontSizes[fontSizeIndex];
+            setIsDarkMode(true);
+            setTheme(newTheme);
+        }
+        else {
+            var newTheme = lightMode;
+            newTheme.size = fontSizes[fontSizeIndex];
+            setIsDarkMode(false);
+            setTheme(newTheme);
+        }
+        refresh();
+    }
+
+    function renderResult({ item }) {
+        const splittedText = item.excerpt.split(search);
+
+        return (
+            <TouchableOpacity style={style.resultFound} activeOpacity={0.4} onPress={() => goToLocation(item.cfi)}>
+                <Text style={style.resultFoundTitle}>{splittedText[0]}<Text style={style.resultFoundTitleBold}>{search}</Text>{splittedText[1]}</Text>
+            </TouchableOpacity>
+        )
+    }
+
+    return (
+        <View style={style.container}>
+            <View style={style.content}>
+                <View style={style.header}>
+                    <SafeAreaView />
+                    <Icon name="chevron-left" size={25} color="#FFF" />
+                </View>
+                <WebView
+                    ref={webview}
+                    source={html}
+                    originWhitelist={["*"]}
+                    injectedJavaScriptBeforeContentLoaded={injectedJS}
+                    scrollEnabled={false}
+                    onLoadStart={(syntheticEvent) => {
+                        // update component to be aware of loading status
+                        console.log("Start Loading")
+                    }}
+                    onLoadEnd={(syntheticEvent) => {
+                        console.log("End Loading")
+                    }}
+                    onMessage={(event) => {
+                        handleMessage(event)
+                    }}
+                />
+            </View>
+            <View style={style.footer2}>
+                <TextInput placeholder="Busca por palavra" onChangeText={setSearch} style={style.textInput} placeholderTextColor='#111' />
+                <Button title='Buscar' color='#FFF' onPress={goSearch} disabled={search.length > 0 ? false : true} />
+            </View>
+            <View style={style.footer}>
+                <Button title='Anterior' color='#FFF' onPress={goPrev} />
+                <Button title='a-' color='#FFF' onPress={decreaseFontSize} />
+                <Button title='o' color='#FFF' onPress={goDarkMode} />
+                <Button title='A+' color='#FFF' onPress={increaseFontSize} />
+                <Button title='Próxima' color='#FFF' onPress={goNext} />
+            </View>
+            <Modal
+                visible={isModalVisible}
+                animationType="slide"
+                transparent={true}>
+                <View style={style.modalContainer}>
+                    <View style={style.resultsContainer}>
+                        <View style={style.resultHeader}>
+                            <View></View>
+                            <Text style={style.resultTitle}>{searchResults.length} resultado(s) encontrado(s)</Text>
+                            <Button title="Voltar" onPress={() => setIsModalVisible(false)} />
+                        </View>
+                        <FlatList
+                            data={searchResults}
+                            renderItem={renderResult}
+                            keyExtractor={(item, index) => index.toString()} />
+                    </View>
+                </View>
+            </Modal>
+        </View>
+    )
+}
+
+export default Home;
---
+++ .history/src/screens/Home/style_20210112105836.js
@@ -0,0 +1,73 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    content: {
+        flex: 1
+    },
+    header: {
+        paddingHorizontal: 20,
+        paddingBottom: 10,
+        backgroundColor: '#000',
+    },
+    headerContent: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    headerRight: {
+        flexDirection: 'row',
+    },
+    footer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingHorizontal: 20,
+        paddingVertical: 10
+    },
+    footer2: {
+        flexDirection: 'row',
+        paddingHorizontal: 20,
+        paddingVertical: 10
+    },
+    textInput: {
+        flex: 1,
+        backgroundColor: '#FFF',
+        paddingHorizontal: 10,
+        borderRadius: 5
+    },
+    modalContainer: {
+        flex: 1,
+        backgroundColor: 'rgba(0,0,0,0.5)',
+    },
+    resultsContainer: {
+        flex: 1,
+        backgroundColor: '#FFF',
+        marginTop: 300,
+        paddingTop: 20,
+        paddingHorizontal: 20,
+        borderRadius: 20
+    },
+    resultHeader: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: 10
+    },
+    resultTitle: {
+        fontSize: 18
+    },
+    resultFound: {
+        marginBottom: 10,
+        paddingBottom: 10,
+        borderBottomWidth: 0.5,
+        borderBottomColor: 'gray',
+    },
+    resultFoundTitle: {
+        fontSize: 16,
+    },
+    resultFoundTitleBold: {
+        fontSize: 16,
+        fontWeight: 'bold'
+    }
+})

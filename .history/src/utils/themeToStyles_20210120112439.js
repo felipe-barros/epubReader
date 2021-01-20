43b40276409907487690fde87c323a4b4b841af5
Felipe Barros <bcfelipe@icloud.com>
2021-01-20 17:26:48 -0300

    Adicionar notas

---
+++ .history/src/utils/themeToStyles_20210120112439.js
@@ -0,0 +1,53 @@
+let styles = {
+    body: {
+        background: '#fafafa',
+        color: '#121212',
+        'font-size': '100%',
+    },
+    p: {
+        color: '#ffffff',
+        'font-size': '100%',
+    },
+    li: {
+        color: '#ffffff',
+        'font-size': '100%',
+    },
+    h1: {
+        color: '#ffffff',
+    },
+    a: {
+        color: '#FFF'
+    },
+    '[ref="epubjs-mk"]::before': {
+        'content': '""',
+        'background': 'url("data:image/svg+xml;base64,PHN2ZyB2ZXJzaW9uPScxLjEnIHhtbG5zPSdodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZycgeG1sbnM6eGxpbms9J2h0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsnIHg9JzBweCcgeT0nMHB4JyB2aWV3Qm94PScwIDAgNzUgNzUnPjxnIGZpbGw9JyNCREJEQkQnIGlkPSdidWJibGUnPjxwYXRoIGNsYXNzPSdzdDAnIGQ9J00zNy41LDkuNEMxOS42LDkuNCw1LDIwLjUsNSwzNC4zYzAsNS45LDIuNywxMS4zLDcuMSwxNS42TDkuNiw2NS42bDE5LTcuM2MyLjgsMC42LDUuOCwwLjksOC45LDAuOSBDNTUuNSw1OS4yLDcwLDQ4LjEsNzAsMzQuM0M3MCwyMC41LDU1LjQsOS40LDM3LjUsOS40eicvPjwvZz48L3N2Zz4=") no-repeat',
+        'display': 'block',
+        'right': '0',
+        'position': 'absolute',
+        'width': '20px',
+        'height': '20px',
+        'margin': '0',
+        'cursor': 'pointer'
+    }
+};
+
+export default function (theme) {
+    styles.body = {
+        background: theme.bg,
+        color: theme.fg,
+        'font-size': theme.size,
+    };
+    styles.p = {
+        color: theme.fg,
+        'font-size': theme.size,
+    };
+    styles.li = {
+        color: theme.fg,
+        'font-size': theme.size,
+    };
+    styles.a = {
+        color: theme.fg
+    };
+    styles.h1.color = theme.fg;
+    return styles;
+}

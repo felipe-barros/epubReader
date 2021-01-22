53d77fd24906cc3b6816538b435c775c3a902889
Felipe Barros <bcfelipe@icloud.com>
2021-01-22 15:52:48 -0300

    Corrir espaçamento de texto ao passar de página

--- .history/src/utils/themeToStyles_20210122105513.js
+++ .history/src/utils/themeToStyles_20210122105513.js
@@ -2,20 +2,25 @@ let styles = {
     body: {
         background: '#FFF',
         'font-size': '100%',
+        textAlign: 'justify !important'
     },
     p: {
         color: '#ffffff',
         'font-size': '100%',
+        textAlign: 'justify !important'
     },
     li: {
         color: '#ffffff',
         'font-size': '100%',
+        textAlign: 'justify !important'
     },
     h1: {
         color: '#ffffff',
+        textAlign: 'justify !important'
     },
     a: {
         color: '#FFF',
+        textAlign: 'justify !important'
     },
     '::selection': {
         'background': 'lightskyblue'
